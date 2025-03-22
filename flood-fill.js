cc76005bbea8e6442153635efd0735365001b379
Michael Desanker <mdesanker@gmail.com>
2025-03-22 04:58:00 -0500

    Add

--- flood-fill.js
+++ flood-fill.js
@@ -24,3 +24,40 @@ var floodFill = function (image, sr, sc, color) {
 
 // Time: O(rc)
 // Space: O(rc)
+
+// bfs
+var floodFill = function (image, sr, sc, color) {
+  const rows = image.length,
+    cols = image[0].length;
+  const target = image[sr][sc];
+
+  if (target === color) return image;
+
+  const dir = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+  ];
+
+  const q = [[sr, sc]];
+  while (q.length) {
+    const len = q.length;
+    for (let i = 0; i < len; i++) {
+      const [r, c] = q.shift();
+      image[r][c] = color;
+
+      for (const [dr, dc] of dir) {
+        const row = r + dr,
+          col = c + dc;
+        if (row < 0 || row >= rows || col < 0 || col >= cols) continue;
+        if (image[row][col] !== target) continue;
+        q.push([row, col]);
+      }
+    }
+  }
+  return image;
+};
+
+// Time: O(rc)
+// Space: O(rc)
