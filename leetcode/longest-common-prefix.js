cc7cb27fb6fc6dfcbfa60ca3ee0ab8b5e618e5e7
Michael Desanker <mdesanker@gmail.com>
2026-03-05 04:06:47 -0600

    Add

--- leetcode/longest-common-prefix.js
+++ leetcode/longest-common-prefix.js
@@ -12,3 +12,45 @@ var longestCommonPrefix = function (strs) {
 // Time: O(n * m + nlogn) - n * m for nested loops, nlogn for sort
 // Space: O(n) - for sort
 // n = strs.length, m = strs[0].length
+
+var longestCommonPrefix = function (strs) {
+  let pre = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    let curr = "";
+    for (let j = 0; j < Math.min(pre.length, strs[i].length); j++) {
+      if (pre[j] === strs[i][j]) curr += pre[j];
+      else break;
+    }
+    pre = curr;
+  }
+  return pre;
+};
+// TC: O(n * m)
+// SC: O(m)
+
+// Binary search
+var longestCommonPrefix = function (strs) {
+  function isCommonPrefix(strs, len) {
+    let pre = strs[0].slice(0, len);
+    for (let i = 1; i < strs.length; i++) {
+      if (!strs[i].startsWith(pre)) return false;
+    }
+    return true;
+  }
+
+  let minLen = Infinity;
+  for (const str of strs) minLen = Math.min(minLen, str.length);
+  let l = 1,
+    r = minLen;
+  while (l <= r) {
+    const mid = l + Math.floor((r - l) / 2);
+    if (isCommonPrefix(strs, mid)) {
+      l = mid + 1;
+    } else {
+      r = mid - 1;
+    }
+  }
+  return strs[0].slice(0, l - 1);
+};
+// TC: O(n * logm)
+// SC: O(m)
